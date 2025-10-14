18c370a27c0bc56b292823b71c7c3179293c0b6a
Wait For-Summer <58130219+WaitForSummer@users.noreply.github.com>
2025-10-14 16:52:25 +0300

    added tests

---
+++ LW5/LW5.Tests/MyMatrixTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LW5.Tests
+{
+    [TestClass]
+    public class MyMatrixTests
+    {
+        [TestMethod]
+        public void TestConstructor_IsValid()
+        {
+
+        }
+    }
+}
