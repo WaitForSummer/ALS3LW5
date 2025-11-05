68e5e73330d59466922d078fbc0ff7472cd04b17
Wait For-Summer <58130219+WaitForSummer@users.noreply.github.com>
2025-11-06 01:38:23 +0300

    updated MyListTests.cs

--- LW5/LW5/MyDictionaryT.cs
+++ LW5/LW5/MyDictionaryT.cs
@@ -1,50 +1,62 @@
-﻿//using System;
-//using System.Collections;
-//using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
-//namespace LW5
-//{
-//    public class MyDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
-//    {
-//        // init fields
-//        private struct MyObject
-//        {
-//            public int hashCode;
-//            public int nextI;
-//            public TKey key;
-//            public TValue value;
-//        }
+namespace LW5
+{
+    public class MyDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+    {
+        // init fields
+        private struct MyObject
+        {
+            public int hashCode;
+            public int nextI;
+            public TKey key;
+            public TValue value;
+        }
 
-//        private MyObject[] myObjects;
-//        private int[] buckets;
-//        private int count;
-//        private int capacity;
-//        private int freeL;
-//        private int freeC;
+        private MyObject[] myObjects;
+        private int[] buckets;
+        private int count;
+        private int capacity;
+        private int freeL;
+        private int freeC;
 
-//        // constructor
-//        public MyDictionary() : this(0) { }
-//        public MyDictionary(int capacity)
-//        {
-//            // init base params
-//            if (capacity < 0) 
-//            { 
-//                this.capacity = 4; 
-//            }
-//            else
-//            {
-//                this.capacity = capacity;
-//            }
+        // constructor
+        public MyDictionary() : this(0) { }
+        public MyDictionary(int capacity)
+        {
+            // init base params
+            if (capacity < 0)
+            {
+                this.capacity = 4;
+            }
+            else if (capacity == 0)
+            {
+                this.capacity = 4;
+            }
+            else
+            {
+                this.capacity = capacity;
+            }
 
-//            buckets = new int[this.capacity];
-//            myObjects = new MyObject[this.capacity];
+            buckets = new int[this.capacity];
+            myObjects = new MyObject[this.capacity];
 
-//            for (int i = 0; i < this.capacity; i++) 
-//                buckets[i] = -1;
+            for (int i = 0; i < this.capacity; i++)
+                buckets[i] = -1;
 
-//            freeL = -1;
-//            count = 0;
-//            freeC = 0;
-//        }
-//    }
-//}
+            // init of list of free cells 
+            freeL = -1;
+            count = 0;
+            freeC = 0;
+            //filling free cells list
+            for (int i = 0; i < this.capacity; i++)
+            {
+                myObjects[i].nextI = freeL;
+                freeL = i;
+                freeC++;
+            }
+        }
+    }
+}
